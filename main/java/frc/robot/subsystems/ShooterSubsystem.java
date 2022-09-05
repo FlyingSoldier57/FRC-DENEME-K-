6e2bc45226b93184d5644451b1b5f47304ad95e9
GizShi <93006992+GizShi@users.noreply.github.com>
2022-09-05 22:36:52 +0300

    Add files via upload

---
+++ main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.ShooterConstants.SHOOTER_OFF_SPEED;
+import static frc.robot.Constants.ShooterConstants.SHOOTER_ON_SPEED;
+import static frc.robot.Constants.SHOOTER_DRIVE_ID;
+
+public class ShooterSubsystem extends SubsystemBase {
+  
+
+    private final TalonSRX shooterMotor = new TalonSRX(SHOOTER_DRIVE_ID);
+
+  public ShooterSubsystem() {}
+    
+    public void setPosition(boolean run) {
+        if (run){
+            shooterMotor.set(TalonSRXControlMode.PercentOutput, SHOOTER_ON_SPEED);
+        
+        } else {
+            shooterMotor.set(TalonSRXControlMode.PercentOutput, SHOOTER_OFF_SPEED);
+        }
+    }
+
+}
